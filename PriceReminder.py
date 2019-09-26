395d3fb7acf927c2b149c690bab247b8b4390161
CrazyBunQnQ <baobao222222@qq.com>
2019-09-26 21:18:21 +0800

    Optimize reminder

--- PriceReminder.py
+++ PriceReminder.py
@@ -4,6 +4,7 @@ import urllib.request
 import re
 import pymysql
 from datetime import datetime
+from decimal import getcontext, Decimal
 
 # IFTTT Info
 KEY = "your_ifttt_key"
@@ -16,9 +17,9 @@ DB_PWD = "your_db_password"
 DB_NAME = "ICO"
 DB_CHARSET = "utf8mb4"
 # Other Setting
-REMINDER_POINT = 0.05
-LIAN_XU_TIMES = 2
+LIAN_XU_TIMES = 3
 HUO_BI_API_BASE_URL = 'https://api.huobi.pro'
+getcontext().prec = 10
 
 
 # TODO 添加点击通知打开交易 app
@@ -34,21 +35,25 @@ def get_curr_rate(scur="USD", tcur="CNY", amount="1"):
         # print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " " + html)
         lists = re.findall(rex, html)
         if len(lists) > 0:
-            return float(lists[0][2])
+            return Decimal(lists[0][2])
         return 0
     except Exception as e:
-        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " get rate error: " + e)
+        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " get rate error: " + str(e))
         return 0
-    return 0
 
 
 def get_latest_ico_price(name="btc", fiat="usdt"):
     url = HUO_BI_API_BASE_URL + "/market/trade?symbol=" + name + fiat
-    # try:
-    response = requests.get(url)
-    response_json = response.json()
-    # Convert the price to a floating point number
-    return float(response_json['tick']['data'][0]['price'])
+    try:
+        response = requests.get(url)
+        response_json = response.json()
+        # Convert the price to a floating point number
+        return Decimal(response_json['tick']['data'][0]['price'])
+    except Exception as e:
+        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " get last price error: " + str(e))
+        return 0
+
+
 # except:
 #     print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " get price error")
 #     return 0
@@ -72,7 +77,7 @@ def post_ifttt_webhook_img(event, title, message, img_url):
     requests.post(ifttt_event_url, json=data)
 
 
-def send_notice_link(ico, price, times, is_img):
+def send_notice_link(ico, price, reminder_point, times, is_img):
     title = ico + " 价格变动"
     rise_or_fall = "涨"
     buy_or_sell = "脱手"
@@ -80,9 +85,9 @@ def send_notice_link(ico, price, times, is_img):
         rise_or_fall = "跌"
         buy_or_sell = "买入"
     message = ico + " 现在" + rise_or_fall + "到 " + str(price) + " 元啦！相对于上次提醒的价格" + rise_or_fall + "了 " + str(
-        REMINDER_POINT * 100) + "% ！"
+        reminder_point * 100) + "% ！"
     if abs(times) >= 2:
-        point = round(abs(times) * REMINDER_POINT * 100, 1)
+        point = round(abs(times) * reminder_point * 100, 1)
         message += "已连续" + rise_or_fall + "了 " + str(point) + "% 啦!!!~ "
     message += "你要趁现在" + buy_or_sell + "吗？"
     if is_img:
@@ -99,63 +104,134 @@ def query_db_prices():
     try:
         db_connect = pymysql.connect(host=DB_HOST, user=DB_USER, passwd=DB_PWD, db=DB_NAME, charset=DB_CHARSET)
         cursor = db_connect.cursor()
-        sql = "select t.id, t.sample_name, t.price, t.lian_xu from Coin t"
+        # sql = "select t.id, t.sample_name, t.price, t.lian_xu from Coin t"
+        sql = "select t.id, t.symbol, t.coin_name, t.platform, t.max_price, t.min_price, t.reminder_point, t.count_in_a_row, t.trade_avg_price " \
+              "from autotrade.strategy_low_buy_high_sell_max t where t.enabled is true"
         cursor.execute(sql)
         rows = cursor.fetchall()
         db_connect.close()
         for row in rows:
             ico = {}
-            ico['name'] = row[1]
-            ico['price'] = row[2]
-            ico['times'] = row[3]
-            dic[row[0]] = ico
-    except:
-        post_ifttt_webhook_link(EVENT_NAME, "价格提醒脚本出错啦！", "数据库查询出错！有空记得检查一下哟！", "")
+            ico['id'] = row[0]
+            ico['symbol'] = row[1]
+            ico['coin_name'] = row[2]
+            ico['platform'] = row[3]
+            ico['max_price'] = row[4]
+            ico['min_price'] = row[5]
+            ico['reminder_point'] = float(row[6])
+            ico['count_in_a_row'] = row[7]
+            ico['trade_avg_price'] = row[8]
+            dic[row[2] + "_" + row[3]] = ico
+    except Exception as e:
+        post_ifttt_webhook_link(EVENT_NAME, "价格提醒脚本出错啦！", "数据库查询出错！有空记得检查一下哟！" + e, "")
     return dic
 
 
-def update_db_prices(ico, price, times):
+def update_db_prices(symbol_id, last_price, max_price, min_price, times):
     try:
         db_connect = pymysql.connect(host=DB_HOST, user=DB_USER, passwd=DB_PWD, db=DB_NAME, charset=DB_CHARSET)
         cursor = db_connect.cursor()
-        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        sql = "update Coin t set t.price = " + str(price) + ", t.update_time = '" + now + "', t.lian_xu = " + str(
-            times) + " where t.id = '" + ico + "'"
+        sql = "update autotrade.strategy_low_buy_high_sell_max set trade_avg_price = " + str(last_price) + ", max_price = " + str(max_price) + ", min_price = " + \
+              str(min_price) + ", count_in_a_row = " + str(times) + " where id = " + str(symbol_id)
         print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " " + sql)
         cursor.execute(sql)
         db_connect.commit()
         db_connect.close()
-    except:
-        post_ifttt_webhook_link(EVENT_NAME, "价格提醒脚本出错啦！", "数据库更新出错！有空记得检查一下哟！", "")
+    except Exception as e:
+        post_ifttt_webhook_link(EVENT_NAME, "价格提醒脚本出错啦！", "数据库更新出错！有空记得检查一下哟！" + e, "")
+
+
+def is_rebound_rise(cur_usd, max_price, min_price):
+    return max_price - cur_usd > cur_usd - min_price
 
 
 def main():
     dic = query_db_prices()
     rate = 0
     while rate == 0:
-        rate = get_curr_rate()
+        # rate = get_curr_rate()
+        rate = Decimal(7.123300)
         if rate == 0:
             continue
 
         for ico in dic:
+            coin = dic[ico]
             usd = 0
             while usd == 0:
-                usd = get_latest_ico_price(dic[ico]['name'])
-            price = round(usd * rate, 2)
-            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 现在 %s 的价格为 %s 元" % (ico, price))
+                usd = get_latest_ico_price(coin['coin_name'])
 
-            cur_point = (usd - dic[ico]['price']) / dic[ico]['price']
-            if abs(cur_point) > REMINDER_POINT:
-                dic[ico]['price'] = usd
-                times = dic[ico]['times']
-                if times / cur_point > 0:
-                    times = times + int(cur_point / abs(cur_point))
+            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 现在 %s 的价格为 %s 元" % (ico, usd * rate))
+
+            last_price = coin['trade_avg_price']
+            count_in_a_row = coin['count_in_a_row']
+            last_count_in_a_row = coin['count_in_a_row']
+            max_price = coin['max_price']
+            min_price = coin['min_price']
+            reminder_point = coin['reminder_point']
+            is_rebound = False
+
+            cur_point = reminder_point - reminder_point
+            if usd > max_price:
+                max_price = usd
+                cur_point = float(((usd - min_price) / min_price) if last_price == 0 else ((usd - last_price) / last_price))
+                count_in_a_row = int(cur_point / reminder_point)
+                update_db_prices(coin['id'], last_price, max_price, min_price, count_in_a_row)
+            elif usd < min_price:
+                min_price = usd
+                cur_point = float(((usd - max_price) / max_price) if last_price == 0 else ((usd - last_price) / last_price))
+                count_in_a_row = int(cur_point / reminder_point)
+                update_db_prices(coin['id'], last_price, max_price, min_price, count_in_a_row)
+            else:
+                is_rebound = True
+
+            if abs(cur_point) > reminder_point:
+                # coin['price'] = usd
+                if count_in_a_row / cur_point > 0:
+                    count_in_a_row = count_in_a_row + int(cur_point / abs(cur_point))
                 else:
-                    times = int(cur_point / abs(cur_point))
+                    count_in_a_row = int(cur_point / abs(cur_point))
                 if usd != 0:
-                    update_db_prices(ico, usd, times)
-                    send_notice_link(dic[ico]['name'], price, times, False)
+                    update_db_prices(coin['id'], last_price, usd, usd, count_in_a_row)
+                    send_notice_link(coin['coin_name'], usd * rate, reminder_point, count_in_a_row, False)
+            elif is_rebound:
+                # 是否反弹, 若反弹判断为提醒百分比的 1/3
+                reminder_point = reminder_point * 1 / 3
+                if last_price == 0:
+                    isReboundRise = is_rebound_rise(usd, max_price, min_price)
+                    if isReboundRise:
+                        cur_point = float((usd - min_price) / min_price)
+                    else:
+                        cur_point = float((max_price - usd) / max_price)
+                else:
+                    # TODO 根据上次交易价格获取涨跌幅度
+                    print()
+
+                if abs(cur_point) <= reminder_point:
+                    print("cur_point: " + str(cur_point))
+                    continue
+
+                if last_price == 0:
+                    title = coin['coin_name'] + "价格反弹了!"
+                    # TODO 打开 火币 app 的 url
+                    url = ""
+                    if isReboundRise:
+                        message = "近期从 " + str(max_price * rate) + " 元跌到 " + str(min_price * rate) + " 元后反涨了 " + str(cur_point) + "(" + str(usd * rate) + " 元)"
+                        # TODO 进行交易, 获取实际交易金额
+                        # TODO 余额不足电话提醒
+                        update_db_prices(coin['id'], last_price, usd, usd, 0)
+                    else:
+                        message = "近期从 " + str(min_price * rate) + " 元涨到 " + str(max_price) + " 元后反跌了 " + str(cur_point * rate) + "(" + str(usd * rate) + " 元)"
+                        # TODO 进行交易, 获取实际交易金额
+                        # TODO 余额不足电话提醒
+                        update_db_prices(coin['id'], last_price, usd, usd, 0)
+                    print(message)
+                    post_ifttt_webhook_img(EVENT_NAME, title, message, url)
+                else:
+                    # TODO 能赚才交易
+                    print()
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    while True:
+        main()
--- db_update.sql
+++ db_update.sql
@@ -1,23 +1,62 @@
 create table Coin
 (
-  id          int auto_increment
-    primary key,
-  name        varchar(20)                    not null
-  comment '名称',
-  full_name   varchar(255)                   null
-  comment '全称',
-  price       double(7, 5) default '1.00000' null
-  comment '上一次通知价格(美元)',
-  update_time datetime                       null
-  comment '更新时间',
-  constraint Coin_name_uindex
-  unique (name)
+    id          int auto_increment
+        primary key,
+    name        varchar(20)                    not null
+        comment '名称',
+    full_name   varchar(255)                   null
+        comment '全称',
+    price       double(7, 5) default '1.00000' null
+        comment '上一次通知价格(美元)',
+    update_time datetime                       null
+        comment '更新时间',
+    constraint Coin_name_uindex
+        unique (name)
 )
-  comment '货币表';
+    comment '货币表';
 
 # insert what Coin you like
-INSERT INTO `ICO`.`Coin` (`id`, `name`, `sample_name`, `price`, `update_time`) VALUES ('eos', 'EOS', 'EOS', 1, '2018-06-30 15:43:55');
-INSERT INTO `ICO`.`Coin` (`id`, `name`, `sample_name`, `price`, `update_time`) VALUES ('binance-coin', 'Binance Coin', 'BNB', DEFAULT, '2018-06-30 15:52:42');
-INSERT INTO `ICO`.`Coin` (`id`, `name`, `sample_name`, `price`, `update_time`) VALUES ('ripple', 'XRP', 'XRP', 1, '2018-06-30 15:48:40');
+INSERT INTO `ICO`.`Coin` (`id`, `name`, `sample_name`, `price`, `update_time`)
+VALUES ('eos', 'EOS', 'EOS', 1, '2018-06-30 15:43:55');
+INSERT INTO `ICO`.`Coin` (`id`, `name`, `sample_name`, `price`, `update_time`)
+VALUES ('binance-coin', 'Binance Coin', 'BNB', DEFAULT, '2018-06-30 15:52:42');
+INSERT INTO `ICO`.`Coin` (`id`, `name`, `sample_name`, `price`, `update_time`)
+VALUES ('ripple', 'XRP', 'XRP', 1, '2018-06-30 15:48:40');
 
-ALTER TABLE Coin ADD lian_xu int DEFAULT 0 NULL COMMENT '连续涨跌次数';
+ALTER TABLE Coin
+    ADD lian_xu int DEFAULT 0 NULL COMMENT '连续涨跌次数';
+
+CREATE TABLE attribute
+(
+    name               varchar(200) PRIMARY KEY COMMENT '货币全称',
+    sample_name        varchar(10) COMMENT '简写',
+    coinmarketcap_id   varchar(200) COMMENT 'coinmarketcap 平台 id',
+    coinmarketcap_rank int(3) COMMENT 'coinmarketcap 平台排名',
+    otcbtc_id          varchar(20) COMMENT 'otcbtc 平台 id',
+    otcbtc_ticker_id   varchar(20) COMMENT 'otcbtc 平台 ticker id'
+);
+ALTER TABLE attribute
+    COMMENT = '各平台属性对照表';
+
+drop table if exists autotrade.strategy_low_buy_high_sell_max;
+create table autotrade.strategy_low_buy_high_sell_max
+(
+    id              int auto_increment,
+    symbol          varchar(15)                                                           not null,
+    coin_name       varchar(11)                                                           not null,
+    platform        varchar(15)                                                           not null,
+    max_price       decimal(18, 10) default 0                                             not null,
+    min_price       decimal(18, 10) default 0                                             not null,
+    reminder_point  float(4, 3)     default 0.021                                         not null comment '提醒百分比：每涨跌该值时进行提醒',
+    count_in_a_row  int(3)          default 0                                             not null comment '连续次数',
+    trade_spend     decimal(18, 10) default 0                                             not null comment '上次交易实际金额：通过交易后余额与交易前余额做差',
+    trade_quantity  decimal(18, 10) default 0                                             not null comment '上次交易实际数量，通过交易后余额与交易前余额做差',
+    trade_avg_price decimal(18, 10) default 0                                             not null comment '上次交易均价：实际交易金额/实际交易数量',
+    trade_side      varchar(4)      default ''                                            not null comment '交易方向：buy 或 sell',
+    enabled         tinyint(1)      default 0                                             not null comment '上次交易订单号',
+    last_order_id   int             default 0                                             not null,
+    create_time     datetime        default CURRENT_TIMESTAMP                             not null,
+    update_time     datetime        default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null,
+    constraint strategy_low_buy_high_sell_max_pk
+        primary key (id)
+);
